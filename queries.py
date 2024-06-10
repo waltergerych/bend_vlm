1d3cbf6fb34c1ea50c381cae6b876d8fbdec7077
waltergerych <55201742+waltergerych@users.noreply.github.com>
2024-06-10 18:03:40 -0400

    Add files via upload

---
+++ queries.py
@@ -0,0 +1,7 @@
+stereotype_classes = [
+            'addict', 'burglar', 'criminal', 'dangerous person', 'delinquent', 'deviant', 'embezzler',
+            'felon', 'fraud', 'freak', 'gangster', 'hacker', 'illegal person', 'insane person', 'lawless person', 'mugger',
+            'murderer', 'psychopath', 'robber', 'savage', 'shoplifter', 'terrorist', 'thief', 'thug', 'violent person'
+        ]
+
+hair_classes = ['Blond_Hair', 'Black_Hair', 'Brown_Hair', 'Gray_Hair']
